c3ef838a726c51a9eb741a778d231f9aeb4930d3
Hillary Alexandra Cavalcante Siolin <hsiolin.upa@epl.gov.br>
2022-08-02 14:55:17 -0300

    atualização

--- cliente/view/pesquisar.php
+++ cliente/view/pesquisar.php
@@ -12,49 +12,48 @@ if ($nomes) {
     $stmt->execute();
 
     $resultados = $stmt->fetchAll(PDO::FETCH_ASSOC);
+    ?>
 
-?>
-
-<!--ini listagem-->
-<div class="container">
-    <table class="table table-bordered table-striped ">
-        <thead class="thead-dark">
-            <tr class="text-center">
-                <th>#</th>
-                <th>Nome</th>
-                <th>Ultimo nome</th>
-                <th>E-mail</th>
-                <th>Endereço</th>
-                <th>Cidade</th>
-                <th>Data de Nascimento</th>
-                <th>CEP</th>
-                <th>Ações</th>
-            </tr>
-            <?php
-            $i = 0;
-            foreach ($rows as $row) {
-                extract($row);
-                // Editar para formato BR correto
-                $date_br = date('d/m/Y', strtotime($data));
-                $um = substr($cep, 0, 5);
-                $dois = substr($cep, -3);
-                $cep_br = $um . '-' . $dois;
-                $i++;
-               //lista dados
-                echo"<tr>";
-                echo"<td>$i</td>";
-                echo"<td>$nome</td>";
-                echo"<td>$ultimonome</td>";
-                echo"<td>$email</td>";
-                echo"<td>$endereco</td>";
-                echo"<td>$cidade</td>";
-                echo"<td>$date_br</td>";
-                echo"<td>$cep_br</td>";
-                echo"<td><a href='editar.php?id=$id'class='btn btn-success'>Editar</a></td>";
-                echo"<td><a class='btn btn-danger' href=?id=$id>Excluir</a></td>";
-                echo"</tr>";
+    <!--ini listagem-->
+    <div class="container">
+        <table class="table table-bordered table-striped ">
+            <thead class="thead-dark">
+                <tr class="text-center">
+                    <th>#</th>
+                    <th>Nome</th>
+                    <th>Ultimo nome</th>
+                    <th>E-mail</th>
+                    <th>Endereço</th>
+                    <th>Cidade</th>
+                    <th>Data de Nascimento</th>
+                    <th>CEP</th>
+                    <th>Ações</th>
+                </tr>
+                <?php
+                $i = 0;
+                foreach ($rows as $row) {
+                    extract($row);
+                    // Editar para formato BR correto
+                    $date_br = date('d/m/Y', strtotime($data));
+                    $um = substr($cep, 0, 5);
+                    $dois = substr($cep, -3);
+                    $cep_br = $um . '-' . $dois;
+                    $i++;
+                    //lista dados
+                    echo"<tr>";
+                    echo"<td>$i</td>";
+                    echo"<td>$nome</td>";
+                    echo"<td>$ultimonome</td>";
+                    echo"<td>$email</td>";
+                    echo"<td>$endereco</td>";
+                    echo"<td>$cidade</td>";
+                    echo"<td>$date_br</td>";
+                    echo"<td>$cep_br</td>";
+                    echo"<td><a href='editar.php?id=$id'class='btn btn-success'>Editar</a></td>";
+                    echo"<td><a class='btn btn-danger' href=?id=$id>Excluir</a></td>";
+                    echo"</tr>";
+                }
             }
-}
             ?>
 
     </table>
