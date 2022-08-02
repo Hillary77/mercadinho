9aa4f545907d9b493fd997e19bef577bf1802ff5
Hillary Alexandra Cavalcante Siolin <hsiolin.upa@epl.gov.br>
2022-08-02 15:42:30 -0300

    alteração do código da venda

--- venda/insert.php
+++ venda/insert.php
@@ -5,9 +5,6 @@ include "../_app/config.php";
 //Recupera as informações preenchidas no formulário que foi trago pelo metódo POST.
 $dados = filter_input_array(INPUT_POST, FILTER_SANITIZE_STRING);
 $dados['public'] = date('Y-m-d H:i:m');
-var_dump($dados);
-exit;
-
 unset($dados['salvar']);
 
 //Gera codigo da venda
